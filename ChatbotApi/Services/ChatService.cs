51fbc05108439068f927bb2d1191e0250bc7bb54
bharti2415 <bgbhartigupta17@gmail.com>
2025-09-01 18:29:34 +0530

    Create ChatService.cs

---
+++ ChatbotApi/Services/ChatService.cs
@@ -0,0 +1,35 @@
+using Azure.AI.OpenAI;
+using Azure;
+
+namespace ChatbotApi.Services
+{
+    public class ChatService
+    {
+        private readonly OpenAIClient _client;
+        private readonly string _deploymentName;
+
+        public ChatService(IConfiguration configuration)
+        {
+            var endpoint = new Uri(configuration["AzureOpenAI:Endpoint"]);
+            var apiKey = new AzureKeyCredential(configuration["AzureOpenAI:ApiKey"]);
+            _deploymentName = configuration["AzureOpenAI:DeploymentName"];
+            _client = new OpenAIClient(endpoint, apiKey);
+        }
+
+        public async Task<string> GetResponseAsync(string userInput)
+        {
+            var chatCompletionsOptions = new ChatCompletionsOptions
+            {
+                Messages =
+                {
+                    new ChatMessage(ChatRole.System, "You are a helpful chatbot."),
+                    new ChatMessage(ChatRole.User, userInput)
+                },
+                MaxTokens = 200
+            };
+
+            var response = await _client.GetChatCompletionsAsync(_deploymentName, chatCompletionsOptions);
+            return response.Value.Choices[0].Message.Content;
+        }
+    }
+}
